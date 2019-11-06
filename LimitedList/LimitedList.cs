7c563692fb6ed51049db22a0eba862db343d8e88
Rocangus <hunter_sniper112@hotmail.com>
2019-11-06 14:35:36 +0100

    Added a LimitedList project

---
+++ LimitedList/LimitedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LimitedList 
+{
+    class LimitedList<T> : IEnumerable<T>
+    {
+        private readonly int capacity;
+        private readonly List<T> list;
+        public int Count => list.Count;
+        public bool IsFull => capacity <= Count;
+        public LimitedList(int capacity)
+        {
+            this.capacity = capacity;
+            list = new List<T>();
+        }
+            
+        public bool Add(T  item)
+        {
+            if (IsFull) return false;
+            list.Add(item);
+            return true;
+        }
+
+        public bool Remove(T item) => list.Remove(item);
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            //return list.GetEnumerator();
+            foreach (T item in list)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ LimitedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LimitedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
